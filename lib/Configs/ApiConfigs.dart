1cc844728340cbb3a62631f6a39d5813bfa1f97f
Akhil Biju <akhilbiju66666@gmail.com>
2026-02-20 15:13:16 +0530

    Commit after login api integration

---
+++ lib/Configs/ApiConfigs.dart
@@ -0,0 +1,18 @@
+class ApiConfig {
+  // This is the Test Server URL
+  static String baseUrl =
+      "https://dev-api.airotrack.in/airotrack-api/public/website/";
+
+  // This is the base Image URL
+  static String imageUrl =
+      "https://ourworks.co.in/saimpex-backend/public/storage/";
+
+
+}
+
+class ApiEndPoints {
+  static String login = "login";
+  static String home = "home";
+  static String alerts = "alerts";
+  static String vehicleHistory = "track_vehicle";
+}
